4484af56a1c68f2de303a630eae5d49e0d16104e
slightlycyborg <collinalexbell@gmail.com>
2019-12-29 01:36:36 +0000

    rm tutorial comments

--- public/js/routine.js
+++ public/js/routine.js
@@ -1,7 +1,5 @@
-// Define the `phonecatApp` module
 var routineApp = angular.module('routineApp', []);
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
 routineApp.controller('GoalListController', function GoalListController($scope, $http) {
   $scope.name = window.location.pathname.replace("/routine/", "")
   $http.get(window.location.pathname + ".json")
