ec900b107fd56cb3bcef0a733387bd8e82629e59
slightlycyborg <collinalexbell@gmail.com>
2020-01-13 14:08:54 +0000

    Filter routines by social and obsolete

--- routine.js
+++ routine.js
@@ -9,7 +9,9 @@ function loadRoutines(dirname, levels) {
       loadRoutines(dirname + dirent.name + '/', levels.concat(dirent.name))
     } else if (dirent.isFile() && dirent.name.charAt(0) != '.') {
       path = levels.concat(dirent.name).join("/")
-      if(!path.includes("obsolete")) routines.push({path: path})
+      if(!path.includes("obsolete") && !path.includes("social")) {
+        routines.push({path: path})
+      }
     }
   }
 }
